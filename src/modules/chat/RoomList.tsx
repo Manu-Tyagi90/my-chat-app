35be99417e364529dc59651367b4313a73a0a9a7
Manu-Tyagi90 <manutygii90@gmail.com>
2025-08-11 16:56:34 +0530

    Fix: remove unused default React imports from chat components

--- src/modules/chat/RoomList.tsx
+++ src/modules/chat/RoomList.tsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 type Props = {
   rooms: string[];
   selectedRoom: string;
--- src/modules/chat/TypingIndicator.tsx
+++ src/modules/chat/TypingIndicator.tsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 type Props = {
   typingUser: string | null;
 };
