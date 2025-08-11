dbaafa73a81d7deec8700d1aa004936c2da5fcf7
Manu-Tyagi90 <manutygii90@gmail.com>
2025-08-11 23:27:27 +0530

    Material UI Design

--- src/services/socket.ts
+++ src/services/socket.ts
@@ -1,6 +1,6 @@
 // src/services/socket.ts
 import io from "socket.io-client";
-//export const socket = io("https://chat-application-socket-io-vite-latest.onrender.com", {
-export const socket = io("http://localhost:3000", {
+export const socket = io("https://chat-application-socket-io-vite-latest.onrender.com", {
+// export const socket = io("http://localhost:3000", {
   autoConnect: false,
 });
